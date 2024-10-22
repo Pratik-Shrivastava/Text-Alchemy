bb02a1d859bf15d90c7c920361d942636ddb579f
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-10-22 16:54:00 +0530

    feat: initialize backend project

---
+++ server/text_alchemy/src/main/java/com/titan/text_alchemy/TextAlchemyApplication.java
@@ -0,0 +1,13 @@
+package com.titan.text_alchemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAlchemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAlchemyApplication.class, args);
+	}
+
+}
---
+++ server/text_alchemy/src/test/java/com/titan/text_alchemy/TextAlchemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titan.text_alchemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAlchemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
