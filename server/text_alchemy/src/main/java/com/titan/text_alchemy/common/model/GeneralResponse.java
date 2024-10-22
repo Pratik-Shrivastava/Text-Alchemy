3b969128fd985072702d60e44ba88df5734f8466
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-10-22 18:44:33 +0530

    feat: add General Response model

---
+++ server/text_alchemy/src/main/java/com/titan/text_alchemy/common/model/GeneralResponse.java
@@ -0,0 +1,18 @@
+package com.titan.text_alchemy.common.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GeneralResponse {
+	
+	boolean status;
+	int status_code;
+	String message;
+	Object data;
+}
