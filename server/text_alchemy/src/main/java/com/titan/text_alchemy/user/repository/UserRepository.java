33f7a0bce33fc76e9864456ea538f97fa483181c
Pratik-Shrivastava <pratikkumar441@gmail.com>
2024-10-23 01:32:01 +0530

    feat: add user  repository

---
+++ server/text_alchemy/src/main/java/com/titan/text_alchemy/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.titan.text_alchemy.user.repository;
+
+import com.titan.text_alchemy.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
